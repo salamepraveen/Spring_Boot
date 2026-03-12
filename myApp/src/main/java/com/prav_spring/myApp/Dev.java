42b7b07bc262780dbd5b312616b9eab9d5574639
salamepraveen <salamepraveen15@gmail.com>
2026-03-12 19:30:53 +0530

    Practicing  SPring

---
+++ myApp/src/main/java/com/prav_spring/myApp/Dev.java
@@ -0,0 +1,12 @@
+package com.prav_spring.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build() {
+        System.out.println("from dev class");
+    }
+
+}
